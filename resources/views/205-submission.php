46f5be0c0df15b56dc98637bd6941c19d2b36895
GoAwayPeter <gaw.undefined@gmail.com>
2016-03-15 11:37:33 +0000

    stuff

--- resources/views/205-submission.php
+++ resources/views/205-submission.php
@@ -8,22 +8,38 @@
             display: flex;             /* NEW, Spec - Opera 12.1, Firefox*/
             align-items: center;
             flex-direction: column;
+            font-family:verdana;
+            font-size:18px;
         }
         .titleBox{
-            background:tomato;
+            font-size:22px;
         }
         .contentBox{
-            background:tomato;
+            flex-direction: row;
+            text-align: center;
         }
         .leftContainer{
             flex-direction: column;
             float: left;
-            background:tomato;
+            justify-content: space-around;
         }
         .rightContainer{
             flex-direction: column;
             float: right;
-            background:tomato;
+            justify-content: space-around;
+        }
+        .item{
+            background: tomato;
+            padding: 5px;
+            width: 200px;
+            height: 150px;
+            margin-top: 10px;
+            
+            line-height: 150px;
+            color: white;
+            font-weight: bold;
+            font-size: 3em;
+            text-align: center;
         }
     </style>
     <title>
@@ -34,15 +50,68 @@
     </head>
     <body>
     <div class="box">
-        <li class="titleBox">
-            Technology: the good, the bad, and the ugly
-        </li>
-        <li class="contentBox">
-            <li class="leftContainer">
-            </li>
-            <li class="rightContainer">
-            </li>
-        </li>
+        <div class="titleBox">
+            <strong>Technology: the good, the bad, (<del>and the ugly</del>)</strong><br>
+        </div>
+        <div class="contentBox">
+            A brief list of the ways in which technology has caused problems within our <br>
+            society, as well as ways it has helped.<br>
+            <div class="leftContainer">
+                <strong>The good</strong>
+                <div class="item">
+                    1
+                </div>
+                <div class="item">
+                    2
+                </div>
+                <div class="item">
+                    3
+                </div>
+                <div class="item">
+                    4
+                </div>
+                <div class="item">
+                    5
+                </div>
+                <div class="item">
+                    6
+                </div>
+                <div class="item">
+                    7
+                </div>
+                <div class="item">
+                    8
+                </div>
+            </div>
+            <div class="rightContainer">
+                <strong>The bad</strong>
+                <div class="item">
+                    1
+                </div>
+                <div class="item">
+                    2
+                </div>
+                <div class="item">
+                    3
+                </div>
+                <div class="item">
+                    4
+                </div>
+                <div class="item">
+                    5
+                </div>
+                <div class="item">
+                    6
+                </div>
+                <div class="item">
+                    7
+                </div>
+                <div class="item">
+                    8
+                </div>
+                
+            </div>
+        </div>
     </div>
     </body>
 </html>
